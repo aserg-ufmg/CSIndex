673ebe7b868ccc6083ca55f7c90b267653cf6b9b
Marco Tulio Valente <mtvalente@gmail.com>
2018-09-01 17:11:56 -0300

    Ignoring csv, txt, and gz in utils

--- utils/papers_count.py
+++ utils/papers_count.py
@@ -8,58 +8,66 @@ import xmltodict
 import re
 import gzip
 import time
-    
+
 ## constants
 
 FIRST_YEAR= 2013
 LAST_YEAR= 2018
-min_paper_size = 10
+min_paper_size = 4
 
 count = 0
 papers = {}
 
-def paperSize(dblp_pages):
-  page= re.split(r"-|:", dblp_pages)
-  if len(page) == 2:
-     p1= page[0]
-     p2= page[1]
-     return int(p2) - int(p1) + 1
-  elif len(page) == 4:
-     p1= page[1]
-     p2= page[3]
-     return int(p2) - int(p1) + 1
-  elif len(page) == 3:   
-     p1= page[1]
-     p2= page[2]
-     return int(p2) - int(p1) + 1
-  else:
+def paperSize(dblp_pages, url):
+  try:
+    page= re.split(r"-|:", dblp_pages)
+    if len(page) == 2:
+       p1= page[0]
+       p2= page[1]
+       return int(p2) - int(p1) + 1
+    elif len(page) == 4:
+       p1= page[1]
+       p2= page[3]
+       return int(p2) - int(p1) + 1
+    elif len(page) == 3:
+       p1= page[1]
+       p2= page[2]
+       return int(p2) - int(p1) + 1
+    else:
+      print url
+      return 0
+  except:
+     print url
      return 0
 
 
-def parse_dblp_xml(_, dblp_xml):
+def parse_dblp(_, dblp):
     global count, papers, journals
-        
+
     count += 1
-    if count % 20000 == 0:
+    if count % 10000 == 0:
        print str(count)
-              
-    if 'year' in dblp_xml:
-       syear= dblp_xml['year']
+
+    if 'year' in dblp:
+       syear= dblp['year']
        year = int(syear)
-       
+
        if ((year >= FIRST_YEAR) and (year <= LAST_YEAR)):
-          if 'journal' in dblp_xml:
-              journal = dblp_xml['journal']
+          if 'journal' in dblp:
+              journal = dblp['journal']
               if journal in journals:
-                 if 'pages' in dblp_xml:
-                    dblp_pages = dblp_xml['pages']
-                    size = paperSize(dblp_pages)
+                 if 'pages' in dblp:
+                    pages = dblp['pages']
+                    if 'ee' in dblp:
+                       size = paperSize(pages, dblp['ee'])
+                    else:
+                       size = paperSize(pages, "null")
                     if (size >= min_paper_size):
-                       print syear + ":" + journal
-                       papers[journal][year - 2013] += 1  
+                       # print syear + ":" + journal
+                       papers[journal][year - 2013] += 1
     return True
-           
- 
+
+
 # main program
 
 start_time = time.time()
@@ -68,9 +76,9 @@ journals= {}
 with open("journals.txt") as file:
    journals = file.read().splitlines()
 for journal in journals:
-    papers[journal] = [0, 0, 0, 0, 0, 0]   
+    papers[journal] = [0, 0, 0, 0, 0, 0]
 
-xmltodict.parse(gzip.GzipFile('dblp-fixed.xml.gz'), item_depth=2, item_callback=parse_dblp_xml)
+xmltodict.parse(gzip.GzipFile('dblp-fixed.xml.gz'), item_depth=2, item_callback=parse_dblp)
 
 f = open("out.csv",'w')
 for journal in journals:
@@ -86,8 +94,8 @@ for journal in journals:
     f.write(',')
     f.write(str(papers[journal][4]))
     f.write(',')
-    f.write(str(papers[journal][5])) 
-    f.write('\n')    
+    f.write(str(papers[journal][5]))
+    f.write('\n')
 f.close()
 
 end_time = time.time()
